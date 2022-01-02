84d5792099ae13bd5c80347041bd3c760778d41c
SpaghettiCodeJungle <Spaghetticodej@gmail.com>
2022-01-02 14:38:46 +0100

    Updated data in timeline

--- src/data/timelineElements.js
+++ src/data/timelineElements.js
@@ -3,22 +3,31 @@ let timelineElements = [
       id: 1,
       name: "Java SE 8",
       extra: "LTS version",
-      description:
-        "A huge release with many new features",
-      features: [{featureName: "Stream API", website: "https:\\www.google.com"},{featureName: "Lambda Expressions", website: "https:\\www.google.com"},{featureName:"Date and Time API", website: "https:\\www.google.com"},{featureName: "Base64 encoding and decoding", website: "https:\\www.google.com"},{featureName: "Optional Class", website: "https:\\www.google.com"},],  
+      description:"A huge release with many new features.",
+      features: [
+        {
+          featureName: "", 
+          website: ""
+        }],  
       buttonText: "Java 8 feature Examples",
-      website: "https://github.com/SpaghettiCodeJungle",
+      website: "https://github.com/stars/SpaghettiCodeJungle/lists/java-8",
       releaseDate: "March 18, 2014",
       icon: "lts",
-    },
- /*   {
+    },/*
+    {
         id: 2,
         name: "Java SE 9",
         extra: "",
         description:
           "A huge release with many new features",
+          features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 9 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "September 21, 2017",
         icon: "standard",
       },
@@ -28,8 +37,14 @@ let timelineElements = [
         extra: "",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],  
         buttonText: "Java 10 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "March 20, 2018",
         icon: "standard",
       },
@@ -39,8 +54,14 @@ let timelineElements = [
         extra: "LTS version",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 11 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "September 25, 2018",
         icon: "lts",
       },
@@ -50,8 +71,14 @@ let timelineElements = [
         extra: "",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 12 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "March 19, 2019",
         icon: "standard",
       },
@@ -61,8 +88,14 @@ let timelineElements = [
         extra: "",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 13 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "September 17, 2019",
         icon: "standard",
       },  
@@ -72,8 +105,14 @@ let timelineElements = [
         extra: "",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 14 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "March 17, 2020",
         icon: "standard",
       },  
@@ -83,8 +122,14 @@ let timelineElements = [
         extra: "",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 15 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "September 15, 2020",
         icon: "standard",
       },  
@@ -94,22 +139,34 @@ let timelineElements = [
         extra: "",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 16 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "March 16, 2021",
         icon: "standard",
       },  
       {
         id: 10,
         name: "Java SE 17",
-        extra: "",
+        extra: "LTS version",
         description:
           "A huge release with many new features",
+        features: [
+            {          
+              featureName: "",
+                website: ""
+
+            }],
         buttonText: "Java 17 feature Examples",
-        website: "",
+        website: "https://github.com/SpaghettiCodeJungle",
         releaseDate: "September 14, 2021",
         icon: "lts",
-      },*/       
+      },*/      
   ];
   
   export default timelineElements;
